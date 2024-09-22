bfb33c994e6346a223c407049085899fa025adc5
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-09-22 10:49:33 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import express from "express";
-const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoute from "./routes/user.route";
+import restaurantRoute from "./routes/restaurant.route";
+import menuRoute from "./routes/menu.route";
+import orderRoute from "./routes/order.route";
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`server is listeing at port ${port}`);
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// default middleware
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+
+// cors
+app.use(
+  cors({
+    origin: true, // Allow all origins
+    credentials: true, // Allow credentials (cookies, authorization headers) cross-origin
+  })
+);
+
+// api
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/restaurant", restaurantRoute);
+app.use("/api/v1/menu", menuRoute);
+app.use("/api/v1/order", orderRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
 });
